7715abfc8acb1d9af92af3ab0bfb97871236c3c5
Hanna2525 <bir2114066@maricopa.edu>
2024-08-30 04:26:19 -0700

    Test Case

---
+++ src/test/java/com/sandbox/calculator/calculatorTest.java
@@ -0,0 +1,50 @@
+package com.sandbox.calculator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class calculatorTest {
+
+    @Test
+    void addTwoNumbers() {
+
+        assertEquals(10, calculator.add(5, 5));
+        assertEquals(0,calculator .add(-5, 5));
+        assertEquals(-10, calculator.add(-5, -5));
+
+    }
+
+    @Test
+    void addMultipleNumbers() {
+
+        assertEquals(15, calculator.add(5, 5, 5));
+        assertEquals(0, calculator.add(0, 0, 0));
+        assertEquals(-15, calculator.add(-5, -5 - 5));
+
+    }
+
+    @Test
+    void subtract() {
+
+        assertEquals(0, calculator.subtract(5, 5));
+        assertEquals(-10, calculator.subtract(-5, 5));
+        assertEquals(0, calculator.subtract(0, 0));
+    }
+
+    @Test
+    void multiply() {
+
+        assertEquals(25, calculator.multiply(5, 5));
+        assertEquals(-25, calculator.multiply(-5, 5));
+        assertEquals(0, calculator.multiply(0, 5));
+    }
+
+    @Test
+    void divide() {
+
+        assertEquals(1, calculator.divide(5, 5));
+        assertEquals(-1, calculator.divide(-5, 5));
+        assertEquals(0, calculator.divide(0, 5));
+        assertThrows(ArithmeticException.class,()-> calculator.divide(5,0));
+    }
+}
